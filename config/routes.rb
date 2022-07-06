c1ae01bd7cf910b3d4109c3027d2a33e1731078f
Cristobal Dominguez <cristobald@gmail.com>
2022-07-05 20:54:40 -0400

    Genera controladores de User

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
   resources :posts
-  devise_for :users
+  
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   root 'posts#index'
 end
